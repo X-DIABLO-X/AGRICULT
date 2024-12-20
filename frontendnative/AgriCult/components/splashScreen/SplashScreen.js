ff84eacd28bd88aa6372bd0dca75691e55eca7e7
Ankit <ankitkr.ak14@gmail.com>
2024-12-20 12:54:19 +0530

    m

--- frontendnative/AgriCult/components/splashScreen/SplashScreen.js
+++ frontendnative/AgriCult/components/splashScreen/SplashScreen.js
@@ -1,13 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const SplashScreen = () => {
   return (
     <View>
-      <Text>Agricult
-      </Text>
+      <Text>Agricult</Text>
+      <Text>Connecting Farmers</Text>
+      
     </View>
-  )
-}
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;
